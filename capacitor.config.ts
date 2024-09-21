c83044092173fa1cffef670d6bb9ae53f161eda0
John Rooney <rooneyjohn@gmail.com>
2024-09-21 10:17:21 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'calApp2',
+  webDir: 'dist'
+};
+
+export default config;
